9580faf628e8a943aaa48746473cd6f91ab69743
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2023-02-08 22:21:55 +0330

    Update useRandomColorPair.ts

--- lib/hooks/useRandomColorPair.ts
+++ lib/hooks/useRandomColorPair.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
-import { Tuple } from 'types';
+//import { Tuple } from 'types';
 
-export function useRandomColorPair(): Tuple<string> {
-  const colors: Tuple<string>[] = [
+export function useRandomColorPair(): string[] {
+  const colors: string[][] = [
     ['#F5E1FF', '#CAF0F8', '#FF8D84', '#FFE599', '#93C47D'],
     ['#EAF4F4', '#FFEDD8', '#76A5AF', '#EC407A', '#BA68C8'],
     ['#F9E5D8', '#EDE9F6', '#64B5F6', '#BCAAA4', '#FF8A65'],
