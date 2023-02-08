d3c51b4ae94c557dbf0fdb3ade40801e85229e0d
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2023-02-08 19:35:36 +0330

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -57,7 +57,7 @@ export default function AuthorLayout({ children, frontMatter }: Props) {
                   strokeWidth={2}
                   color={resumeColor}
                 >
-                  Resume
+                  CV (Curriculum Vitae)
                 </RoughNotation>
               </a>
               <h2 className='mt-8 mb-4 text-2xl font-semibold dark:text-white'>
