7e944f2d05b8b5ccd0bbaf2a851b609b0e6de780
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2023-02-08 20:49:49 +0330

    Update useRandomColorPair.ts

--- lib/hooks/useRandomColorPair.ts
+++ lib/hooks/useRandomColorPair.ts
@@ -3,10 +3,10 @@ import { Tuple } from 'types';
 
 export function useRandomColorPair(): Tuple<string> {
   const colors: Tuple<string>[] = [
-    ['#F5E1FF', '#CAF0F8'],
-    ['#EAF4F4', '#FFEDD8'],
-    ['#F9E5D8', '#EDE9F6'],
-    ['#EEEBFF', '#FFFAD4'],
+    ['#F5E1FF', '#CAF0F8', '#FF8D84', '#FFE599', '#93C47D'],
+    ['#EAF4F4', '#FFEDD8', '#76A5AF', '#EC407A', '#BA68C8'],
+    ['#F9E5D8', '#EDE9F6', '#64B5F6', '#BCAAA4', '#FF8A65'],
+    ['#EEEBFF', '#FFFAD4', '#FFEE58', '#BDBDBD', '#B2FF59'],
   ];
 
   const random = Math.round(Math.random() * (colors.length - 1));
