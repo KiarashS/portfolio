ef0357a89c66bdfa97730fa282038217dbd0d411
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2023-02-09 01:27:39 +0330

    Update stack.ts

--- config/stack.ts
+++ config/stack.ts
@@ -2,14 +2,23 @@ import { Colors } from './colors';
 
 export enum Stack {
   // Languages
-  go,
+  python,
+  java,
+  csharp,
+  r,
   typescript,
   javascript,
-  python,
+  bash,
+  powershell,
+  cpp,
+  vbnet,
+  matlab,
+  latex,
 
   // Frontend
   react,
   reactnative,
+  web,
 
   // Backend
   graphql,
@@ -19,6 +28,7 @@ export enum Stack {
   // Cloud
   aws,
   gcp,
+  azure,
 
   // Messaging
   nats,
@@ -29,30 +39,58 @@ export enum Stack {
   redis,
   postgres,
   mongo,
+  mssql,
 
   // Tools
   docker,
   kubernetes,
+  jenkins,
   terraform,
+  git,
+  
+  // Frameworks
+  scikit,
+  tensorflow,
+  pytorch,
 }
 
 export const WorkStack = [
-  Stack.go,
-  Stack.typescript,
   Stack.python,
+  Stack.java,
+  Stack.csharp,
+  Stack.r,
+  Stack.cpp,
+  Stack.bash,
+  Stack.powershell,
+  Stack.vbnet,
+  Stack.matlab,
+  Stack.latex,
+  Stack.typescript,
+  Stack.javascript,
   Stack.react,
+  Stack.reactnative,
   Stack.aws,
   Stack.gcp,
+  Stack.azure,
   Stack.kubernetes,
+  Stack.jenkins,
   Stack.docker,
   Stack.terraform,
+  Stack.git,
   Stack.nats,
   Stack.kafka,
   Stack.graphql,
   Stack.postgres,
   Stack.redis,
+  Stack.mongo,
   Stack.arangodb,
-  Stack.reactnative,
+  Stack.mssql,
+  Stack.web,
+  Stack.tensorflow,
+  Stack.pytorch,
+  Stack.scikit,
+  Stack.node,
+  Stach.django,
 ];
 
 type StackInfoMap = {
@@ -61,6 +99,46 @@ type StackInfoMap = {
 };
 
 export const StackInfo: Record<Stack, StackInfoMap> = {
+  [Stack.python]: {
+    value: 'Python',
+    color: Colors.python,
+  },
+  [Stack.java]: {
+    value: 'Java',
+    color: Colors.java,
+  },
+  [Stack.csharp]: {
+    value: 'C#',
+    color: Colors.csharp,
+  },
+  [Stack.r]: {
+    value: 'R',
+    color: Colors.r,
+  },
+  [Stack.cpp]: {
+    value: 'C/CPP',
+    color: Colors.cpp,
+  },
+  [Stack.bash]: {
+    value: 'Bash',
+    color: Colors.bash,
+  },
+  [Stack.powershell]: {
+    value: 'Powershell',
+    color: Colors.powershell,
+  },
+  [Stack.vbnet]: {
+    value: 'VB.NET',
+    color: Colors.vbnet,
+  },
+  [Stack.matlab]: {
+    value: 'MATLAB',
+    color: Colors.matlab,
+  },
+  [Stack.latex]: {
+    value: 'LaTeX',
+    color: Colors.latex,
+  },
   [Stack.typescript]: {
     value: 'TypeScript',
     color: Colors.typescript,
@@ -68,10 +146,6 @@ export const StackInfo: Record<Stack, StackInfoMap> = {
   [Stack.javascript]: {
     value: 'JavaScript',
     color: Colors.javascript,
-  },
-  [Stack.go]: {
-    value: 'Go',
-    color: Colors.go,
   },
   [Stack.react]: {
     value: 'React',
@@ -81,10 +155,6 @@ export const StackInfo: Record<Stack, StackInfoMap> = {
     value: 'React Native',
     color: Colors.reactnative,
   },
-  [Stack.graphql]: {
-    value: 'GraphQL',
-    color: Colors.graphql,
-  },
   [Stack.aws]: {
     value: 'AWS',
     color: Colors.aws,
@@ -93,17 +163,29 @@ export const StackInfo: Record<Stack, StackInfoMap> = {
     value: 'Google Cloud',
     color: Colors.gcp,
   },
-  [Stack.python]: {
-    value: 'Python',
-    color: Colors.python,
+  [Stack.azure]: {
+    value: 'Azure',
+    color: Colors.azure,
   },
-  [Stack.node]: {
-    value: 'Node',
-    color: Colors.node,
+  [Stack.kubernetes]: {
+    value: 'Kubernetes',
+    color: Colors.kubernetes,
   },
-  [Stack.django]: {
-    value: 'Django',
-    color: Colors.django,
+  [Stack.jenkins]: {
+    value: 'Jenkins',
+    color: Colors.jenkins,
+  },
+  [Stack.docker]: {
+    value: 'Docker',
+    color: Colors.docker,
+  },
+  [Stack.terraform]: {
+    value: 'Terraform',
+    color: Colors.terraform,
+  },
+  [Stack.git]: {
+    value: 'Git',
+    color: Colors.git,
   },
   [Stack.nats]: {
     value: 'NATS',
@@ -113,9 +195,9 @@ export const StackInfo: Record<Stack, StackInfoMap> = {
     value: 'Kafka',
     color: Colors.kafka,
   },
-  [Stack.arangodb]: {
-    value: 'ArangoDB',
-    color: Colors.arangodb,
+  [Stack.graphql]: {
+    value: 'GraphQL',
+    color: Colors.graphql,
   },
   [Stack.postgres]: {
     value: 'Postgres',
@@ -129,16 +211,36 @@ export const StackInfo: Record<Stack, StackInfoMap> = {
     value: 'MongoDB',
     color: Colors.mongo,
   },
-  [Stack.docker]: {
-    value: 'Docker',
-    color: Colors.docker,
+  [Stack.arangodb]: {
+    value: 'ArangoDB',
+    color: Colors.arangodb,
   },
-  [Stack.kubernetes]: {
-    value: 'Kubernetes',
-    color: Colors.kubernetes,
+  [Stack.mssql]: {
+    value: 'MS SQL',
+    color: Colors.mssql,
   },
-  [Stack.terraform]: {
-    value: 'Terraform',
-    color: Colors.terraform,
+  [Stack.web]: {
+    value: 'Web',
+    color: Colors.web,
+  },
+  [Stack.tensorflow]: {
+    value: 'TensorFlow',
+    color: Colors.tensorflow,
+  },
+  [Stack.pytorch]: {
+    value: 'Py Torch',
+    color: Colors.pytorch,
+  },
+  [Stack.scikit]: {
+    value: 'scikit-learn',
+    color: Colors.scikit,
+  },  
+  [Stack.node]: {
+    value: 'Node',
+    color: Colors.node,
+  },
+  [Stack.django]: {
+    value: 'Django',
+    color: Colors.django,
   },
 };
