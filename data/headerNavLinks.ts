155ef81b2ece48f3f29c76cef49c28261666bada
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2023-02-12 14:03:18 +0330

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: 'https://www.kiarashs.ir', title: 'Profile' },
+  { href: 'https://profile.kiarashs.ir', title: 'Profile' },
   { href: 'https://resource.kiarashs.ir', title: 'Resources' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
