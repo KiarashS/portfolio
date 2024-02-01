c38510459901521241d266f3fd62273e74d2e851
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2024-02-01 20:51:18 +0330

    feat

--- pages/presentations.tsx
+++ pages/presentations.tsx
@@ -14,14 +14,34 @@ function Presentations(): React.ReactElement {
         description={siteMetadata.description}
       />
       <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
-        <Header title='Presentations' />
+        <Header title='Presentations'
+                subtitle="Some of my presentations and talks" />
         <div className='container py-12'>
-          <p>
-            Some of my informative presentations' slides:
-            <ul>
-              <li>Clean Code</li>
-              <li>Regular Expressions</li>
-            </ul>
+          {/* <p> */}
+          <div className="flex h-screen items-center justify-center bg-white px-6 md:px-60">
+            <div className="space-y-6 border-l-2 border-dashed">
+              <div className="relative w-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+                  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                </svg>
+                <div className="ml-6">
+                  <h4 className="font-bold text-blue-500"><a href='#'>Clean Code</a></h4>
+                  <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Comming soon...</p>
+                  {/* <span className="mt-1 block text-sm font-semibold text-blue-500">2024</span> */}
+                </div>
+              </div>
+              <div className="relative w-full">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-blue-500">
+                  <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+                </svg>
+                <div className="ml-6">
+                  <h4 className="font-bold text-blue-500"><a href='#'>Regular Expressions for data scientists</a></h4>
+                  <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Comming soon...</p>
+                  {/* <span className="mt-1 block text-sm font-semibold text-blue-500">2024</span> */}
+                </div>
+              </div>
+            </div>
+          </div>
             {/* <span
               className='ml-2 cursor-pointer !font-normal !text-black !no-underline dark:!text-white'
               role='button'
@@ -39,7 +59,7 @@ function Presentations(): React.ReactElement {
                 schedule a meeting
               </RoughNotation>
             </span> */}
-          </p>
+          {/* </p> */}
         </div>
       </div>
     </>
