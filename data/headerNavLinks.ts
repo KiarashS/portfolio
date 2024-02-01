960019fc016f48762059a93ae725b65fbd3907f5
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2024-02-01 19:06:31 +0330

    feat: added presentation page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/presentations', title: 'Presentations' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
 ];
---
+++ pages/presentations.tsx
@@ -0,0 +1,49 @@
+import { Header } from '@/components/Form';
+import { PageSEO } from '@/components/SEO';
+import siteMetadata from '@/data/siteMetadata';
+// import { useRandomColorPair } from '@/lib/hooks/useRandomColorPair';
+// import { RoughNotation } from 'react-rough-notation';
+
+function Presentations(): React.ReactElement {
+  // const [randomColor] = useRandomColorPair();
+
+  return (
+    <>
+      <PageSEO
+        title={`Presentations - ${siteMetadata.author}`}
+        description={siteMetadata.description}
+      />
+      <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
+        <Header title='Presentations' />
+        <div className='container py-12'>
+          <p>
+            Some of the informative presentations' slides:
+            <ul>
+              <li>Clean Code</li>
+              <li>Regular Expressions</li>
+            </ul>
+            {/* <span
+              className='ml-2 cursor-pointer !font-normal !text-black !no-underline dark:!text-white'
+              role='button'
+              tabIndex={0}
+            >
+              <RoughNotation
+                show
+                type='underline'
+                strokeWidth={2}
+                animationDelay={250}
+                animationDuration={2000}
+                color={randomColor}
+                multiline={true}
+              >
+                schedule a meeting
+              </RoughNotation>
+            </span> */}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Presentations;
