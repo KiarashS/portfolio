f7d034403ed493f712e4ee0291ccc9cb43cbcdf5
Kiarash Soleimanzadeh <1054134+KiarashS@users.noreply.github.com>
2024-02-01 21:50:43 +0330

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
-  { href: '/presentations', title: 'Presentations' },
   { href: '/about', title: 'About' },
   { href: '/contact', title: 'Contact' },
 ];
