7f087a6bf3bf335433c009909b0044618964ff64
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2024-02-09 13:09:25 +0330

    fix

--- next.config.js
+++ next.config.js
@@ -5,12 +5,12 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com www.google-analytics.com cdn.jsdelivr.net;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app www.googletagmanager.com www.google-analytics.com www.googletagmanager.com googleads.g.doubleclick.net cdn.jsdelivr.net;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
+  font-src 'self' blob: data: gap:;
   frame-src giscus.app www.youtube.com calendly.com drawsql.app
 `;
 
