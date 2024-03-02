c5bb7332e39ae9a53b8734c69ce7977d6e74d745
Kiarash Soleimanzadeh <kiarash.s@hotmail.com>
2024-03-02 11:09:46 +0330

    fix

--- pages/slides.tsx
+++ pages/slides.tsx
@@ -4,23 +4,23 @@ import siteMetadata from '@/data/siteMetadata';
 // import { useRandomColorPair } from '@/lib/hooks/useRandomColorPair';
 // import { RoughNotation } from 'react-rough-notation';
 
-function Presentations(): React.ReactElement {
+function Slides(): React.ReactElement {
   // const [randomColor] = useRandomColorPair();
 
   return (
     <>
       <PageSEO
-        title={`Presentations - ${siteMetadata.author}`}
+        title={`Slides - ${siteMetadata.author}`}
         description={siteMetadata.description}
       />
       <div className='fade-in divide-y-2 divide-gray-100 dark:divide-gray-800'>
-        <Header title='Presentations'
-                subtitle="Some of my presentations and talks" />
+        <Header title='Slides'
+                subtitle="Some helpful slides to easily learn and review them" />
         <div className='container py-12 px-6'>
           {/* <p> */}
           <ul className="list-disc">
-            <li>Clean Code [Comming soon...]</li>
             <li>Regular Expressions for data scientists [Comming soon...]</li>
+            <li>Clean Code [Comming soon...]</li>
           </ul>
             {/* <span
               className='ml-2 cursor-pointer !font-normal !text-black !no-underline dark:!text-white'
@@ -46,4 +46,4 @@ function Presentations(): React.ReactElement {
   );
 }
 
-export default Presentations;
+export default Slides;
