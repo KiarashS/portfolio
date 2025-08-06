75354b2a5d9661a0ef9ef5eb7b988f8f8ac238f8
Kiarash Soleimanzadeh <1054134+KiarashS@users.noreply.github.com>
2025-08-07 02:44:52 +0330

    fix

--- pages/slides.tsx
+++ pages/slides.tsx
@@ -43,7 +43,7 @@ function Slides(): React.ReactElement {
           {/* </p> */}
         </div>
 
-          <div className='p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex' role='alert'>
+          <div className='p-2 bg-indigo-800 items-center text-indigo-100 leading-none rounded-full flex lg:inline-flex' role='alert'>
             <span className='flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3'>News</span>
             <span className='font-semibold mr-2 text-left flex-auto'>Please check out <a href='https://directory.kiarashs.ir/' target='_blank' rel='noreferrer' className='font-medium text-orange-600 dark:text-blue-500'>my directory</a> for awesome links and files also!</span>
           </div>
